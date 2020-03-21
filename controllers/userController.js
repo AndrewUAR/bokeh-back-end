d82fea230112bf3a29a5eb8692bc79e68ba43b4c
Andrew <andrewriznykua93@gmail.com>
2020-03-21 13:49:52 -0500

    handlerFactory imported

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,32 +1,12 @@
 const User = require('../models/userModel');
-const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
 
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: {
-      users
-    }
-  });
-});
+exports.getAllUsers = factory.getAll(User);
 
-exports.createUser = catchAsync(async (req, res, next) => {
-  console.log(req.body);
-  const newUser = await User.create({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    role: req.body.role,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm,
-    passwordChangedAt: req.body.passwordChangeAt
-  });
-  res.status(201).json({
-    status: 'success',
-    data: {
-      user: 'Success'
-    }
-  });
-});
+exports.getUser = factory.getOne(User);
+
+exports.createUser = factory.createOne(User);
+
+exports.updateUser = factory.updateOne(User);
+
+exports.deleteUser = factory.deleteOne(User);
