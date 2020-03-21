1ee74d4278126986636933962f39ac5cfb7da7f5
Andrew <andrewriznykua93@gmail.com>
2020-03-21 17:04:02 -0500

    JWT errors handled, module exported

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-console */
 const AppError = require('../utils/appError');
 
-const handleCastErrorDb = err => {
+const handleCastErrorDB = err => {
   const message = `Invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
 };
@@ -17,6 +18,12 @@ const handleValidationErrorDB = err => {
   const message = `Invalid input data. ${errors.join('. ')}`;
   return new AppError(message, 400);
 };
+
+const handleJWTError = err =>
+  new AppError('Invalid token. Please log in again!', 401);
+
+const handleJWTExpiredError = err =>
+  new AppError('Your token has expired! Please log in again!', 401);
 
 const sendErrorDev = (err, req, res) => {
   if (req.originalUrl.startsWith('/api/')) {
@@ -61,4 +68,24 @@ const sendErrorProd = (err, req, res) => {
     title: 'Something went wrong',
     msg: 'Please try again later.'
   });
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, req, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    // eslint-disable-next-line node/no-unsupported-features/es-syntax
+    let error = { ...err };
+    error.message = err.message;
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+    if (error.name === 'TokenExpiredError')
+      error = handleJWTExpiredError(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError(error);
+    sendErrorProd(error, req, res);
+  }
 };
