e4ec02154f306130c64d79563788f6ab637f488a
Andrew <andrewriznykua93@gmail.com>
2020-03-22 23:15:36 -0500

    update password function added

--- controllers/authController.js
+++ controllers/authController.js
@@ -67,31 +67,64 @@ exports.logout = (req, res) => {
   res.status(200).json({ status: 'success' });
 };
 
-exports.isLoggedIn = async (req, res, next) => {
-  if (req.cookies.jwt) {
-    try {
-      const decoded = await promisify(jwt.verify)(
-        req.cookies.jwt,
-        process.env.JWT_SECRET
-      );
+// exports.isLoggedIn = async (req, res, next) => {
+//   if (req.cookies.jwt) {
+//     try {
+//       const decoded = await promisify(jwt.verify)(
+//         req.cookies.jwt,
+//         process.env.JWT_SECRET
+//       );
 
-      const currentUser = await User.findById(decoded.id);
-      if (!currentUser) {
-        return next();
-      }
+//       const currentUser = await User.findById(decoded.id);
+//       if (!currentUser) {
+//         return next();
+//       }
 
-      if (currentUser.changedPasswordAfter(decoded.iat)) {
-        return next();
-      }
+//       if (currentUser.changedPasswordAfter(decoded.iat)) {
+//         return next();
+//       }
 
-      res.locals.user = currentUser;
-      return next();
-    } catch (error) {
-      return next();
-    }
+//       res.locals.user = currentUser;
+//       return next();
+//     } catch (error) {
+//       return next();
+//     }
+//   }
+//   next();
+// };
+
+exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401)
+    );
+  }
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError('The user belonging to this token does no longer exists.')
+    );
+  }
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please log in again.', 401)
+    );
+  }
+  req.user = currentUser;
+  res.locals.user = currentUser;
   next();
-};
+});
 
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
@@ -164,3 +197,15 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
   createSendToken(user, 200, req, res);
 });
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password');
+  if (!user.correctPassword(req.body.passwordCurrent, user.password)) {
+    return next(new AppError('Your current password is wrong', 401));
+  }
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+  createSendToken(user, 200, req, res);
+});
+
