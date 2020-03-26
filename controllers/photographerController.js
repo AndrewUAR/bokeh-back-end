d16d112da28523abe340a9ca7550623ab6fdebe7
Andrew <andrewriznykua93@gmail.com>
2020-03-25 23:09:57 -0500

    getAll and getOne refactored with populate

--- controllers/photographerController.js
+++ controllers/photographerController.js
@@ -4,13 +4,18 @@ const User = require('../models/userModel');
 
 exports.getAllPhotographers = factory.getAll(User, 'photographer', {
   path: 'photographer',
-  populate: { path: 'reviews' }
+  select: '-__v',
+  populate: { path: 'reviews albums photoSessions', select: '-__v' }
 });
 
 exports.getPhotographer = factory.getOne(User, {
   path: 'photographer',
-  populate: { path: 'reviews' }
+  populate: { path: 'reviews albums photoSessions' }
 });
+
+exports.setUserId = (req, res, next) => {
+  req.params.user = req.user.id;
+};
 
 exports.createPhotographer = factory.createOne(Photographer);
 
