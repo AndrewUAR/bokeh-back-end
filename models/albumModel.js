01b17d66bde8063949d862b00e2fcd3749cd1a2a
Andrew <andrewriznykua93@gmail.com>
2020-03-26 22:30:59 -0500

    album schema updated

--- models/albumModel.js
+++ models/albumModel.js
@@ -9,10 +9,7 @@ const albumSchema = new mongoose.Schema({
     type: mongoose.Schema.ObjectId,
     ref: 'Photographer'
   },
-  images: {
-    type: [String],
-    required: [true, 'Album can not be empty']
-  },
+  images: [String],
   createdAt: {
     type: Date,
     default: Date.now()
