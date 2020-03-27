86c37d5ea800dfc0dbc6b7e86abe1f10fe357d4b
Andrew <andrewriznykua93@gmail.com>
2020-03-26 22:29:01 -0500

    cloudinary config added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cloudinary = require('cloudinary');
 const rateLimit = require('express-rate-limit');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
@@ -10,6 +11,13 @@ const bookingRouter = require('./routes/bookingRoutes');
 const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 app.use(express.json({ limit: '10kb' }));
 
 if (process.env.NODE_ENV !== 'development') {
