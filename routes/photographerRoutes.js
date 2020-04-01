628e4505a94e628e670255f3cb1bb15bbb128db8
Andrew <andrewriznykua93@gmail.com>
2020-03-31 22:06:00 -0500

    photographer routes completely refactored

--- routes/photographerRoutes.js
+++ routes/photographerRoutes.js
@@ -1,18 +1,38 @@
 const express = require('express');
 const photographerController = require('../controllers/photographerController');
 const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
 
 const router = express.Router();
 
-router
-  .route('/')
-  .get(photographerController.getAllPhotographers)
-  .post(authController.protect, photographerController.createPhotographer);
+router.route('/').get(photographerController.getAllPhotographers);
+router.route('/:id').get(photographerController.getPhotographer);
 
-router
-  .route('/:id')
-  .get(photographerController.getPhotographer)
-  .patch(authController.protect, photographerController.updatePhotographer)
-  .delete(authController.protect, photographerController.deletePhotographer);
+router.use(authController.protect);
+
+router.route(
+  '/createProfile',
+  authController.restrictTo('photographer'),
+  userController.getMe,
+  photographerController.createPhotographerProfile
+);
+router.route(
+  '/updateProfile',
+  authController.restrictTo('user'),
+  userController.getMe,
+  photographerController.updatePhotographerProfile
+);
+router.route(
+  '/activateProfile',
+  authController.restrictTo('photographer'),
+  userController.getMe,
+  photographerController.activateProfile
+);
+router.route(
+  '/hideProfile',
+  authController.restrictTo('user'),
+  userController.getMe,
+  photographerController.deactivateProfile
+);
 
 module.exports = router;
