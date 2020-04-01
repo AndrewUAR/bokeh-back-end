f2ad7f173df64d6c7e8ea1445bf3ad05ee51861a
Andrew <andrewriznykua93@gmail.com>
2020-03-31 22:10:38 -0500

    send email functionality added

---
+++ utils/email.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
+
+const sendEmail = async options => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
+
+  const mailOptions = {
+    from: 'Do not respond to this letter <hello@bokeh.io>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message
+    // html:
+  };
+
+  // 3) Actually send the email
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
