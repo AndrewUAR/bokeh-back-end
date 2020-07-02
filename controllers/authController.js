6a8640aef34ec3e872d38a65d2f168f66716e421
Andrew <andrewriznykua93@gmail.com>
2020-07-02 13:32:35 -0500

    cookies update

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,6 +17,7 @@ const signToken = id => {
 const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
   res.cookie('jwt', token, {
+    domain: 'www.mypanorama.netlify.app',
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
