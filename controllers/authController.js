daf49d3bb250c84f3fdf0e2a7ef5aa9e31826760
Andrew <andrewriznykua93@gmail.com>
2020-07-16 22:48:31 -0500

    domain deleted

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,7 +19,6 @@ const createSendToken = (user, statusCode, req, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    domain: '.mypanorama.netlify.app',
     httpOnly: true,
     secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
   });
